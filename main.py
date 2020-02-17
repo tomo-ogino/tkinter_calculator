2e6c2db98f28747ed3ad1b1fddf1ad2357338e6d
tomo-ogino <ogino257@gmail.com>
2020-02-17 12:32:51 +0900

    create Push class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.font as font
 import tkinter.ttk as ttk
+from lib.calc import *
 
 
 class Frame(ttk.Frame):
@@ -9,7 +10,8 @@ class Frame(ttk.Frame):
         self.create_display(0,0)
         self.createbutton_num(1,0)
         self.createbutton_ope(1,3)
-    
+
+    #ディスプレイ作成
     def create_display(self, argr, argc):
         d_font = font.Font(family="System",size=12,weight="bold")
         r = argr
@@ -31,21 +33,18 @@ class Frame(ttk.Frame):
             gridc = argc
             while c < len(nu):
                 n = str(num[r][c])
-                button = tk.Button(text=n,font=n_font,width=10,height=3)
-                button.bind("<Button-1>",lambda x: self.push(n))
+                button = tk.Button(root,text=n,font=n_font,width=10,height=3,command=Push(n,"num"))
                 button.grid(row=(gridr) ,column=(gridc) ,padx=20,pady=20)
                 c += 1   
                 gridc += 1
             r += 1
             gridr += 1
         #0
-        button = tk.Button(text=0,font=n_font,width=26,height=3)
-        button.bind("<Button-1>",lambda x: self.push("0"))
+        button = tk.Button(root, text=0,font=n_font,width=26,height=3, command=Push("0","num"))
         button.grid(row=gridr,column=argc,columnspan=2,padx=20,pady=20)
 
         #C
-        button = tk.Button(text="c",font=n_font,width=12,height=3)
-        button.bind("<Button-1>",lambda x: self.push("a"))
+        button = tk.Button(root, text="c",font=n_font,width=12,height=3, command=Push("C","ope"))
         button.grid(row=gridr,column=argc + 2, columnspan=1,padx=20,pady=20)
     
     #演算子ボタン作成
@@ -57,15 +56,25 @@ class Frame(ttk.Frame):
         c = 0
         gridc = argc
         while r < len(ope):
-            button = tk.Button(text=ope[r],font=o_font,width=10,height=3)
-            button.bind("<Button-1>",lambda x: self.push(ope[r]))
+            button = tk.Button(root, text=ope[r],font=o_font,width=10,height=3,command=Push(ope[r],"ope"))
             button.grid(row=gridr,column=gridc, padx=20,pady=20)
             r += 1
             gridr += 1           
 
+#Pushクラスを更新
+class Push:
+    def __init__(self,str,ope):
+        self.name = str
+        if ope == "ope":
+            self.ope = True
+        else:
+            self.ope = False
     #ボタンクリック
-    def push(self,str):
-        print(str)
+    def __call__(self, event=None):
+        if self.ope:
+            print("クリア")
+        else:
+            print(self.name)
 
 if __name__ == '__main__':
   root = tk.Tk()
