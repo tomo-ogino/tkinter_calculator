36264f4bff30a3b4b57406e4b698dc31dc68ed08
tomo-ogino <ogino257@gmail.com>
2020-02-23 17:55:08 +0900

    20200223_01update

--- README.md
+++ README.md
@@ -18,10 +18,15 @@ I use this repository as an exercise for my python programs and Git commands.
 ### 2020/2/17
 * main.pyにPushクラスを作成
 * ボタンのコマンドからPushクラスを呼び出すことでボタンのテキスト取得可能
+### 2020/2/23
+* main.pyの数字ボタン、演算子ボタンを **for r in range** にて呼び出せるように調整
 
 ---
 
 ## ***[problem]***
 ### 2020/2/12
 * ~~すべての数字ボタン押下時に'3'が実行される~~　-> 2020/2/17対応完了
-* ボタン作成をfor in rangeに書き換え
+### 2020/2/17
+* ~~ボタン作成をfor in rangeに書き換え~~ -> 2020/2/23対応完了
+
+---
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import tkinter as tk
 import tkinter.font as font
 import tkinter.ttk as ttk
@@ -5,54 +6,51 @@ from lib.calc import *
 
 
 class Frame(ttk.Frame):
-    def __init__(self, master=None, **kwargs):
+    def __init__(self, master=None, num="0", **kwargs):
         super().__init__(master, **kwargs)
-        self.create_display(0,0)
+        self.var = tk.StringVar()
+        self.var.set(num)
+        self.create_display(num,0,0)
         self.createbutton_num(1,0)
         self.createbutton_ope(1,3)
 
     #ディスプレイ作成
-    def create_display(self, argr, argc):
+    def create_display(self, num, r, c):
         d_font = font.Font(family="System",size=12,weight="bold")
-        r = argr
-        c = argc
-        labelframe = tk.LabelFrame(bd=2,relief="ridge",text="calculate result")
-        labelframe.grid(row=r, column=c, columnspan=4)
+        #labelframe = tk.LabelFrame(bd=2,relief="ridge",text="calculate result")
+        #labelframe.grid(row=r, column=c, columnspan=4)
         var = tk.StringVar()
-        var.set("0")
-        label = tk.Label(labelframe,textvariable=var, font = d_font, width=60,height=3)
+        var.set(num)
+        label = tk.Label(root,textvariable=var, font = d_font, width=60,height=3)
         label.grid(row=r, column=c, columnspan=4)
         
     #数字ボタン作成
     def createbutton_num(self, argr, argc):
         n_font = font.Font(family="System",size=10,weight="bold")
         num = ((7,8,9),(4,5,6),(1,2,3))
-        r = 0
-        gridr = argr
-        while r < len(num):
-            nu = num[r]
-            c = 0
-            gridc = argc
-            while c < len(nu):
+        for r in range(0,3):
+            for c in range(0,3):
                 n = str(num[r][c])
                 button = tk.Button(root,text=n,font=n_font,width=10,height=3,command=Push(n,"num"))
-                button.grid(row=(gridr) ,column=(gridc) ,padx=20,pady=20)
-                c += 1   
-                gridc += 1
-            r += 1
-            gridr += 1
+                button.grid(row=(r + argr) ,column=(c + argc) ,padx=20,pady=20)
+        
         #0
         button = tk.Button(root, text=0,font=n_font,width=26,height=3, command=Push("0","num"))
-        button.grid(row=gridr,column=argc,columnspan=2,padx=20,pady=20)
+        button.grid(row=argr+3,column=argc,columnspan=2,padx=20,pady=20)
 
-        #C
+        #C（クリア）
         button = tk.Button(root, text="c",font=n_font,width=12,height=3, command=Push("C","ope"))
-        button.grid(row=gridr,column=argc + 2, columnspan=1,padx=20,pady=20)
+        button.grid(row=argr+3,column=argc + 2, columnspan=1,padx=20,pady=20)
     
     #演算子ボタン作成
     def createbutton_ope(self, argr, argc):
         o_font = font.Font(family="System",size=10,weight="bold")
         ope = ("÷","×","-","+")
+        for r in range(0, len(ope)):
+            button = tk.Button(root, text=ope[r],font=o_font,width=10,height=3,command=Push(ope[r],"ope"))
+            button.grid(row=r + argr,column=argc, padx=20,pady=20)
+
+        '''
         r = 0
         gridr = argr
         c = 0
@@ -62,8 +60,9 @@ class Frame(ttk.Frame):
             button.grid(row=gridr,column=gridc, padx=20,pady=20)
             r += 1
             gridr += 1           
+        '''
 
-#
+#Labelクラス
 class Label:
     def __init__(self, name):
         self.name = name
@@ -71,7 +70,7 @@ class Label:
         pass
 
 #Pushクラス
-class Push:
+class Push(Frame):
     def __init__(self, str, ope):
         self.name = str
         if ope == "ope":
@@ -86,7 +85,7 @@ class Push:
             self.append_num()
     
     def append_num(self):
-        Frame(root).create_display(ver.get()+"1",0,0)
+        super().create_display(super().var().get()+self.name,0,0)
 
 if __name__ == '__main__':
   root = tk.Tk()
